c0f49820440c057bf569c1f7fa496acb643c50e0
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-02-05 10:53:30 +0100

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/admin/login/login.component';
+import { PagesComponent } from './pages/admin/pages/pages.component';
+import { MediaComponent } from './pages/admin/media/media.component';
+import { PostsComponent } from './pages/admin/posts/posts.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'admin/login', component: LoginComponent },
+  { path: 'admin/pages', component: PagesComponent },
+  { path: 'admin/media', component: MediaComponent },
+  { path: 'admin/posts', component: PostsComponent },
 ];
 
 @NgModule({
--- src/app/pages/admin/media/media.component.ts
+++ src/app/pages/admin/media/media.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-media',
   templateUrl: './media.component.html',
-  styleUrls: ['./media.component.scss']
+  styleUrls: ['./media.component.scss'],
 })
 export class MediaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
