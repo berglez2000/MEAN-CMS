14a87e9fdba211c79188c13cb877052fad6468ce
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-02-05 09:01:40 +0100

    Inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
