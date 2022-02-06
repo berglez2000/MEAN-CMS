b65a898aa9badea40bacb9de5e0be9a773c381c8
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-02-06 11:40:22 +0100

    frontend login

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Alert } from 'src/app/models/Alert';
 import { AlertService } from 'src/app/services/alert/alert.service';
 
 @Component({
@@ -14,5 +15,15 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    if (!this.username || !this.password) {
+      const alert: Alert = this.alertService.createAlert(
+        'warning',
+        'please fill in the username and password',
+        2000
+      );
+      this.alertService.addAlert(alert);
+      return;
+    }
+  }
 }
