abcda2a830a3b8b1bc221ede825df3df55f5b077
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-02-07 07:38:50 +0100

    frontend pages

--- src/app/models/Page.ts
+++ src/app/models/Page.ts
@@ -2,6 +2,7 @@ export interface Page {
   _id?: any;
   title: string;
   content: string;
+  slug: string;
 }
 
 export interface PageObject {
--- src/app/pages/admin/pages/create-page/create-page.component.ts
+++ src/app/pages/admin/pages/create-page/create-page.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Alert } from 'src/app/models/Alert';
+import { Page } from 'src/app/models/Page';
+import { AlertService } from 'src/app/services/alert/alert.service';
+import { PagesService } from 'src/app/services/api/pages/pages.service';
 
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.scss']
+  styleUrls: ['./create-page.component.scss'],
 })
 export class CreatePageComponent implements OnInit {
+  title: string = '';
+  content: string = '';
+  slug: string = '';
 
-  constructor() { }
+  constructor(
+    private alertService: AlertService,
+    private pagesService: PagesService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  notFilledCorrectly(): void {
+    const alert: Alert = {
+      type: 'warning',
+      text: 'Please fill the title',
+      time: 2000,
+    };
+    this.alertService.addAlert(alert);
   }
 
+  onSubmit(): void {
+    if (!this.title) {
+      this.notFilledCorrectly();
+      return;
+    }
+
+    this.slug = this.pagesService.stringToSlug(this.title);
+
+    const page: Page = {
+      title: this.title,
+      content: this.content,
+      slug: this.slug,
+    };
+
+    this.title = '';
+    this.content = '';
+  }
 }
