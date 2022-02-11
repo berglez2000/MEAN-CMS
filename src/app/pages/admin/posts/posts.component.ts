8bf967ef10e285d55b0911ebd857d202e801e3de
berglez2000 <aljaz.berglez@gmail.com>
2022-02-11 06:59:35 +0100

    frontend posts

--- src/app/pages/admin/posts/posts.component.ts
+++ src/app/pages/admin/posts/posts.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from 'src/app/models/Page';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  isLoading: boolean = true;
+  posts: Page[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
