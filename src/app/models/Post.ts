e3f9bb273c59709850c29d02adcdbf1b534bb035
berglez2000 <aljaz.berglez@gmail.com>
2022-02-15 07:20:49 +0100

    frontend deleting posts

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -1,4 +1,5 @@
 export interface Post {
+  _id?: any;
   title: string;
   image: string;
   content: string;
--- src/app/pages/admin/posts/post-item/post-item.component.ts
+++ src/app/pages/admin/posts/post-item/post-item.component.ts
@@ -1,7 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
+import { Alert } from 'src/app/models/Alert';
 import { Post } from 'src/app/models/Post';
+import { ServerResponse } from 'src/app/models/ServerResponse';
+import { AlertService } from 'src/app/services/alert/alert.service';
 import { PostsService } from 'src/app/services/api/posts/posts.service';
 
 @Component({
@@ -15,7 +19,8 @@ export class PostItemComponent implements OnInit {
 
   constructor(
     private modalService: NgbModal,
-    private postService: PostsService
+    private postService: PostsService,
+    private alertService: AlertService
   ) {}
 
   ngOnInit(): void {}
@@ -30,5 +35,31 @@ export class PostItemComponent implements OnInit {
 
   onDelete(): void {
     this.modalService.dismissAll();
+
+    this.subscription = this.postService.deletePost(this.post._id).subscribe(
+      (res: ServerResponse) => {
+        if (res.success) {
+          const alert: Alert = {
+            type: 'success',
+            text: 'Post deleted successfully',
+            time: 2000,
+          };
+          this.alertService.addAlert(alert);
+        }
+      },
+      (error: HttpErrorResponse) => {
+        let alert: Alert = {
+          type: 'danger',
+          time: 2000,
+          text: '',
+        };
+        if (error.status === 401) {
+          alert.text = 'Not auhtorized. Please login';
+        } else if (error.status === 404) {
+          alert.text = 'Post not found';
+        }
+        this.alertService.addAlert(alert);
+      }
+    );
   }
 }
