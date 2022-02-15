70da2121535da3ecac00c3f411088807f89ef8d7
berglez2000 <aljaz.berglez@gmail.com>
2022-02-15 07:01:58 +0100

    post item

--- src/app/pages/admin/posts/post-item/post-item.component.ts
+++ src/app/pages/admin/posts/post-item/post-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Post } from 'src/app/models/Post';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
-  styleUrls: ['./post-item.component.scss']
+  styleUrls: ['./post-item.component.scss'],
 })
 export class PostItemComponent implements OnInit {
+  @Input() post!: Post;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openBackDropCustomClass(content: any) {
+    this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
   }
 
+  openWindowCustomClass(content: any) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  onDelete(): void {
+    this.modalService.dismissAll();
+  }
 }
